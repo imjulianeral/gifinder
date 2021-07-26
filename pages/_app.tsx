2ddaefda1c4f84f72841130384af116517a8f170
Jorge Acero <julianacero98@gmail.com>
2021-07-25 21:48:09 -0500

    style(setup): general setup completed
    sass, reseter and  directories defined

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
-import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+
+import 'reseter.css'
+import '@styles/globals.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
