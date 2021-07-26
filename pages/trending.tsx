2cd4e3d082f074f791234d4e242457c5cf476206
Jorge Acero <julianacero98@gmail.com>
2021-07-26 13:44:59 -0500

    feat(content): giphy api added on trending page

--- pages/trending.tsx
+++ pages/trending.tsx
@@ -1,6 +1,26 @@
+import { GetServerSideProps } from 'next'
+
 import { Layout } from '@components/ui'
 
-export default function Trending() {
+type TrendingProps = {
+  gifList: object[]
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const uri = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_KEY}`
+  const req = await fetch(uri)
+  const gifList = await req.json()
+
+  return {
+    props: {
+      gifList,
+    },
+  }
+}
+
+export default function Trending({ gifList }: TrendingProps) {
+  console.log(gifList)
+
   return (
     <Layout pageName="Trending" description="Most searched gifs">
       <h1>Trending</h1>
