18ab0c0e59168886d04da287b92cff8490248871
Jorge Acero <julianacero98@gmail.com>
2021-07-26 00:52:14 -0500

    feat(layout): layout component added

---
+++ pages/trending.tsx
@@ -0,0 +1,9 @@
+import { Layout } from '@components/ui'
+
+export default function Trending() {
+  return (
+    <Layout pageName="Trending" description="Most searched gifs">
+      <h1>Trending</h1>
+    </Layout>
+  )
+}
