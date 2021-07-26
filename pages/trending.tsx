ff264ec9ac7c5368c2ca6f40a21f969a3da50939
Jorge Acero <julianacero98@gmail.com>
2021-07-26 14:21:00 -0500

    fix(images): images showing on trending

--- pages/trending.tsx
+++ pages/trending.tsx
@@ -1,29 +1,39 @@
-import { GetServerSideProps } from 'next'
+import type { GetServerSideProps } from 'next'
+
+import Image from 'next/image'
 
 import { Layout } from '@components/ui'
-
-type TrendingProps = {
-  gifList: object[]
-}
+import { TrendingResponse } from '@typeDefs/Giphy'
 
 export const getServerSideProps: GetServerSideProps = async () => {
   const uri = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.GIPHY_KEY}`
   const req = await fetch(uri)
-  const gifList = await req.json()
+  const res: TrendingResponse = await req.json()
 
   return {
     props: {
-      gifList,
+      ...res,
     },
   }
 }
 
-export default function Trending({ gifList }: TrendingProps) {
-  console.log(gifList)
+export default function Trending({ data }: TrendingResponse) {
+  console.log(data)
 
   return (
     <Layout pageName="Trending" description="Most searched gifs">
       <h1>Trending</h1>
+
+      {data.map(gif => (
+        <Image
+          key={gif.id}
+          loader={() => gif.images.fixed_width_small.url}
+          src={gif.images.fixed_width_small.url}
+          alt={gif.title}
+          width={gif.images.fixed_width_small.width}
+          height={gif.images.fixed_width_small.height}
+        />
+      ))}
     </Layout>
   )
 }
