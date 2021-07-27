2e8b6fab2b3cf621c205c0928abd28e626a59630
Jorge Acero <julianacero98@gmail.com>
2021-07-27 17:58:38 -0500

    fix(twitter metatag): twitter metatag fixed

--- components/ui/Layout.tsx
+++ components/ui/Layout.tsx
@@ -28,14 +28,14 @@ export function Layout({ pageName, description, cover, children }: LayoutProps) 
         <meta itemProp="image" content={cover} />
 
         {/* Facebook Meta Tags  */}
-        <meta property="og:url" content="https://www.framer.com" />
+        <meta property="og:url" content="https://gifinder.vercel.app/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={pageName} />
         <meta property="og:description" content={description} />
         <meta property="og:image" content={cover} />
 
         {/* Twitter Meta Tags  */}
-        <meta name="twitter:card" content="gif image" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={pageName} />
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={cover} />
