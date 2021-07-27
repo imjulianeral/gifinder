e59169886ccef9e531825eed2cfad58c1fe529c6
Jorge Acero <julianacero98@gmail.com>
2021-07-27 11:14:35 -0500

    fix(ux): active link styles added

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,25 +1,34 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import { Home, TrendingUp } from 'react-feather'
 
 export function Navbar() {
+  const router = useRouter()
+
   return (
     <nav className="nav container">
       <Link href="/">
-        <a className="nav__logo" aria-label="GiFinder">
+        <a className="nav__logo" aria-label="GiFinder logo">
           GiFinder
         </a>
       </Link>
       <ul className="nav__menu">
         <li className="nav__link">
           <Link href="/">
-            <a aria-label="home link">
+            <a
+              aria-label="home link"
+              className={router.pathname === '/' ? 'nav__link-active' : ''}
+            >
               <Home />
             </a>
           </Link>
         </li>
         <li className="nav__link">
           <Link href="/trending">
-            <a aria-label="trending link">
+            <a
+              aria-label="trending link"
+              className={router.pathname === '/trending' ? 'nav__link-active' : ''}
+            >
               <TrendingUp />
             </a>
           </Link>
